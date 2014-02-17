d526ca91a0b7f320fbf489f33eb5af77e45e7ed3
Robert Roberson <robertmrobersonjr@gmail.com>
2014-02-16 20:16:22 -0500

    finished changing search bars to my custom search bar.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,3 +1,29 @@
+/* 
+ * Copyright (c) 2014, rober_000
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 'use strict';
 
 /* Controllers */
@@ -623,18 +649,17 @@ function GenerateCtrl($scope, $filter, $http, MessageService, HostService, VarSe
   });
   
   $scope.$watch('generate.selectAllMsgs', function(newvalue, oldvalue) {
-    alterFilteredSelect($scope.generate.messages, $scope.generate.msgSearch, newvalue);
+    alterFilteredSelect($scope.generate.filteredMessages, newvalue);
   });
   
   $scope.$watch('generate.selectAllHosts', function(newvalue, oldvalue) {
-    alterFilteredSelect($scope.generate.hosts, $scope.generate.hostSearch, newvalue);
+    alterFilteredSelect($scope.generate.filteredHosts, newvalue);
   });  
   
-  function alterFilteredSelect(items, filter, value) {
+  function alterFilteredSelect(items, value) {
     var f = $filter('filter'); 
-    var selection = f(items,filter);
-    for(var i = 0; i < selection.length; i++) {
-      selection[i].selected = value;
+    for(var i = 0; i < items.length; i++) {
+      items[i].selected = value;
     }
   }
   
